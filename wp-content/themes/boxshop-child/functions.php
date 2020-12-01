2beb0e92c83b95e589b474ddf521a4474592cb6f
kennCK <kennettecanales@gmail.com>
2020-12-01 13:50:28 +0800

    Update functions.php

--- wp-content/themes/boxshop-child/functions.php
+++ wp-content/themes/boxshop-child/functions.php
@@ -9,3 +9,9 @@ function boxshop_child_register_scripts(){
     );
 }
 add_action( 'wp_enqueue_scripts', 'boxshop_child_register_scripts' );
+function cw_change_product_price_display( $price ) {
+    $price .= ' POST & PACKAGE';
+    return $price;
+}
+add_filter( 'woocommerce_get_price_html', 'cw_change_product_price_display' );
+add_filter( 'woocommerce_cart_item_price', 'cw_change_product_price_display' );
